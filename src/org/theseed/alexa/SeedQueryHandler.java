01f90751a145dc2e3686c01172e3c39c61752587
Bruce Parrello <bparrello@figresearch.com>
2016-05-16 20:11:35 -0500

    Initial commit.

---
+++ src/org/theseed/alexa/SeedQueryHandler.java
@@ -0,0 +1,54 @@
+package org.theseed.alexa;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.amazon.speech.speechlet.lambda.SpeechletRequestStreamHandler;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+/**
+ * This class is the handler for an AWS Lambda function powering an Alexa Skills
+ * Kit experience. To set it up, simply set the handler field in the AWS Lambda
+ * console to "org.theseed.alexa.org.theseed.alexaHandler". Upload the resulting zip file to
+ * power your function.
+ */
+public final class SeedQueryHandler extends SpeechletRequestStreamHandler implements RequestHandler<String, String> {
+    private static final Set<String> supportedApplicationIds;
+
+    static {
+        /*
+         * our special application ID
+         */
+        supportedApplicationIds = new HashSet<String>();
+        supportedApplicationIds.add("amzn1.echo-sdk-ams.app.XXXX"); // TODO replace the XXXs
+    }
+
+    public SeedQueryHandler() {
+        super(new SeedQuerySpeechlet(), supportedApplicationIds);
+    }
+
+    @Override
+    /** Request handler: json in, json out */
+    public String handleRequest(String input, Context context) {
+        String retVal;
+        InputStream inStream = new ByteArrayInputStream(input.getBytes());
+        OutputStream outStream = new ByteArrayOutputStream();
+        try {
+            this.handleRequest(inStream, outStream, context);
+            retVal = new String(outStream.toString());
+        } catch (IOException e) {
+            retVal = null;
+        }
+        return retVal;
+    }
+
+}
+
---
+++ src/org/theseed/alexa/SeedQuerySpeechlet.java
@@ -0,0 +1,86 @@
+package org.theseed.alexa;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.amazon.speech.slu.Intent;
+import com.amazon.speech.speechlet.IntentRequest;
+import com.amazon.speech.speechlet.LaunchRequest;
+import com.amazon.speech.speechlet.Session;
+import com.amazon.speech.speechlet.SessionEndedRequest;
+import com.amazon.speech.speechlet.SessionStartedRequest;
+import com.amazon.speech.speechlet.Speechlet;
+import com.amazon.speech.speechlet.SpeechletException;
+import com.amazon.speech.speechlet.SpeechletResponse;
+
+/**
+ * This class intercepts intents and decides what to do with them.
+ */
+
+public class SeedQuerySpeechlet implements Speechlet {
+    private static final Logger log = LoggerFactory.getLogger(SeedQuerySpeechlet.class);
+
+    /** Default constructor. */
+    public SeedQuerySpeechlet() {
+        initializeComponents();
+    }
+
+    @Override
+    public void onSessionStarted(final SessionStartedRequest request, final Session session) throws SpeechletException {
+        log.info("onSessionStarted requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+
+        // TODO initialize session
+    }
+
+    @Override
+    public SpeechletResponse onLaunch(final LaunchRequest request, final Session session) throws SpeechletException {
+        log.info("onLaunch requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+
+        // TODO compute the launch response.
+        return null;
+    }
+
+    @Override
+    public SpeechletResponse onIntent(IntentRequest request, Session session) throws SpeechletException {
+        Intent intent = request.getIntent();
+        log.info("onIntent requestId={}, sessionId={}, intent={}", request.getRequestId(), session.getSessionId(),
+                intent.getName());
+
+        if ("MyIntent".equals(intent.getName())) {
+            // TODO your first intent. compute a response
+            return null;
+
+        } else if ("AMAZON.HelpIntent".equals(intent.getName())) {
+            // TODO give the user a hint
+            return null;
+
+        } else if ("AMAZON.CancelIntent".equals(intent.getName())) {
+            return getExitIntentResponse(intent, session);
+
+        } else if ("AMAZON.StopIntent".equals(intent.getName())) {
+            return getExitIntentResponse(intent, session);
+
+        } else {
+            throw new IllegalArgumentException("Unrecognized intent: " + intent.getName());
+        }
+    }
+
+    @Override
+    public void onSessionEnded(final SessionEndedRequest request, final Session session) throws SpeechletException {
+        log.info("onSessionEnded requestId={}, sessionId={}", request.getRequestId(), session.getSessionId());
+        // TODO any cleanup logic goes here
+    }
+
+    /**
+     * Initializes the instance components.
+     */
+    private void initializeComponents() {
+        // TODO initialization common to all constructors
+    }
+
+    private SpeechletResponse getExitIntentResponse(Intent intent, Session session) {
+        // TODO say goodbye
+        return null;
+    }
+
+}
